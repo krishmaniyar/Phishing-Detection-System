12a8485f5a8df4b58192929180ae1de10b881c80
KBM <160119279+krishmaniyar@users.noreply.github.com>
2025-08-29 16:28:14 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 if __name__ == "__main__":
@@ -9,7 +10,16 @@ if __name__ == "__main__":
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        logging.info("Starting Data Ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion Completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Starting Data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,26 @@
+import yaml
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import os, sys
+import numpy as np
+# import dill
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def write_yaml_file(file_path: str, content: dict = None, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'w') as yaml_file:
+            yaml.dump(content, yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
